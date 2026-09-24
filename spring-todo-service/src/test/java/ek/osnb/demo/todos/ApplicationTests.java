f1591d9b941b9afc434025062f9dffc071b4c250
osman-butt <butt.osman@gmail.com>
2026-09-25 01:38:16 +0200

    add spring starter

---
+++ spring-todo-service/src/test/java/ek/osnb/demo/todos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ek.osnb.demo.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
